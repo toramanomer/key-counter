8de4c3fcc41ee8fa70d849208aa4959b124df7c6
Ömer Toraman <toramanomer@icloud.com>
2024-11-04 23:03:53 +0300

    feat: implement key counting functionality in KeyCounterController

--- src/key-counter/key-counter.controller.ts
+++ src/key-counter/key-counter.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common'
+import {
+	BadRequestException,
+	Body,
+	Controller,
+	Get,
+	Headers,
+	HttpCode,
+	HttpStatus,
+	Post,
+	Query,
+	UnsupportedMediaTypeException
+} from '@nestjs/common'
 
 @Controller()
-export class KeyCounterController {}
+export class KeyCounterController {
+	private keyCounts = new Map<string, number>()
+
+	/**
+	 * @description
+	 * Handles POST requests to `/input` to add or update a key's occurrence count.
+	 * Does not return a response body, and instead returns a `204 No Content` status code.
+	 *
+	 * @throws {UnsupportedMediaTypeException} If the content type is not `text/plain`.
+	 * @throws {BadRequestException} If the request body is not a string.
+	 */
+	@Post('input')
+	@HttpCode(HttpStatus.NO_CONTENT)
+	handleInput(
+		@Headers('content-type') contentType: string,
+		@Body() body: any
+	): void {
+		if (contentType !== 'text/plain')
+			throw new UnsupportedMediaTypeException()
+
+		if (typeof body !== 'string')
+			throw new BadRequestException('Request body must be a string')
+
+		const count = this.keyCounts.get(body) ?? 0
+		this.keyCounts.set(body, count + 1)
+	}
+
+	/**
+	 * @description
+	 * Handles GET requests to `/query` to retrieve the occurrence count of a key.
+	 *
+	 * @throws {BadRequestException} If the query parameter `key` is not a string.
+	 */
+	@Get('/query')
+	async handleQuery(@Query('key') key: any): Promise<string> {
+		if (typeof key !== 'string')
+			throw new BadRequestException(
+				"Query parameter 'key' must be a string"
+			)
+
+		const count = this.keyCounts.get(key) ?? 0
+		return count.toString()
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core'
+import type { NestExpressApplication } from '@nestjs/platform-express'
 import helmet from 'helmet'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create<NestExpressApplication>(AppModule)
+	app.useBodyParser('text')
 	app.use(helmet())
 	await app.listen(9000)
 }
